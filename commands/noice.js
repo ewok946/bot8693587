d21359cf0e7adb0edd2e9029bb74bc37fc3205d2
adam <adamw5722@gmail.com>
2021-06-08 17:47:37 -0400

    bruh

---
+++ commands/noice.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'noice',
+    description: "this is a ping command!",
+    execute(message, args) {
+        message.channel.send('https://tenor.com/view/noice-nice-click-gif-8843762');
+    }
+}
---
+++ index.js
@@ -0,0 +1,44 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+
+client.once('ready', () => {
+    console.log('Codelyon is online!');
+    client.user.setPresence({ activity: { name: 'pooping' }, status: 'Do Not Disturb' })
+        .then(console.log)
+        .catch(console.error);
+})
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if (command == 'noice') {
+        client.commands.get('noice').execute(message, args);
+    } else if (command == 'ban') {
+        client.commands.get('ban').execute(message, args);
+    } else if (command == 'kick') {
+        client.commands.get('kick').execute(message, args);
+    } else if (command === 'mute') {
+        client.commands.get('mute').execute(message, args);
+    }
+});
+
+client.login('ODQ3NjM2MDg3MTI4MDY0MDMw.YLA8nw.Qz-Qtdx4L8ObYstvywqmjegs_qI');
